e99f2847c7ee9d78697dc74bf6bffb037d9b19c2
Lakshay Arora <lakshay16199@gmail.com>
2026-02-15 18:43:10 +0000

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from textSummaryGenerator.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummaryGenerator.logging import logger
 
-logger.info("This is our first log message from main.py")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
